36e6d9f55054afef9d31124fd0a51d3f4268da2a
craig t smith <craig@42branches.co>
2013-11-27 06:33:45 +0000

    add paperless tool

--- paperless/install.sh
+++ paperless/install.sh
@@ -1,24 +1,24 @@
-# #!/bin/sh
-# echo "\n"
-# #
-# # Paperless
-# #
-# # This istalls the stuff needed for my paperless workflow
+#!/bin/sh
+echo "\n"
+#
+# Paperless
+#
+# This istalls the stuff needed for my paperless workflow
 
-# # Check for Homebrew
-# if test ! $(which brew)
-# then
-#   echo "  Installing Homebrew for you."
-#   ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
-# fi
+# Check for Homebrew
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+  ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+fi
 
-# # Update Brew
-# brew update
+# Update Brew
+brew update
  
-# # Install what we can from brew
-# brew install graphicsmagick poppler ghostscript tesseract 
- 
-# # Install the Doc⚡split gem
-# gem install docsplit
+# We need a custom formula for now
+brew tap craigtsmith/homebrew-additions
 
-# exit 0
+# Install what we can from brew
+brew install exact-image tag tesseract
+
+exit 0
