35477424f593d809b28765cba80bd96b5b3b2dad
agent smith <craigts-agent@users.noreply.github.com>
2026-01-30 13:10:31 +0100

    fix: harden linux bootstrap

--- scripts/install/zsh.sh
+++ scripts/install/zsh.sh
@@ -8,7 +8,7 @@ install_zsh() {
   if ! command_exists zsh; then
     case "$DF_OS_TYPE" in
       macos) spin "Installing zsh" brew install zsh --quiet ;;
-      linux) spin "Installing zsh" bash -c "sudo apt-get update -qq && sudo apt-get install -y -qq zsh" ;;
+      linux) spin "Installing zsh" bash -c "export -f maybe_sudo; maybe_sudo apt-get update -qq && maybe_sudo apt-get install -y -qq zsh" ;;
     esac
     CHANGES_MADE=true
   else
